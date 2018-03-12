d5aa2e70604851876fc9cb61d36f02fbab250389
DESKTOP-0RMEKH8\Hazar Askari <hazaraskari@gmail.com>
2018-03-12 10:15:48 +0100

    no message

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,7 +3,7 @@ import Modal from 'react-modal';
 
 const OptionModal = (props) => (
     <div>
-        <Modal isOpen={!!props.selectedOption} contentLabel="Example Modal">
+        <Modal isOpen={!!props.selectedOption} contentLabel="Example Modal" onRequestClose={props.handleCloseModal}>
             <h1>Modal</h1>
             <p>{props.selectedOption}</p>
             <button onClick={props.handleCloseModal}>Close</button>
